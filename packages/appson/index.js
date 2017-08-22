32b0b30750e69c3d14ce06cd13c4dc934dfeaee4
Pedro Nauck <pedronauck@gmail.com>
2017-08-22 11:34:43 -0300

    feat: add package json to appson lib

---
+++ packages/appson/index.js
@@ -0,0 +1,10 @@
+import appson from './src/appson'
+export default appson
+
+export Link from './src/Link'
+export Route from './src/Route'
+export Routes from './src/Routes'
+
+export model from './src/model'
+export withModel from './src/with-model'
+export withEffects from './src/with-effects'
