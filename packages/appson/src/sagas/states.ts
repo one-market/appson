ac734df613d3524202800b7e1dbb4200f5cb3f31
Pedro Nauck <pedronauck@gmail.com>
2017-09-15 16:53:01 -0300

    fix(appson): syntax

--- packages/appson/src/sagas/states.ts
+++ packages/appson/src/sagas/states.ts
@@ -6,7 +6,7 @@ import State from '../state'
 
 const reducersFrom = (defaultReducers: ReducersMapObject, states: StateMap): ReducersMapObject =>
   R.reduce((reducers: ReducersMapObject, state: State): ReducersMapObject =>
-    R.assoc(state.getStateName(), state.getReducer(), reducers), defaultReducers, R.values(states)
+    R.assoc(state.getName(), state.getReducer(), reducers), defaultReducers, R.values(states)
   )
 
 const statesSaga = (store: AppStore, states: StateMap): void =>
