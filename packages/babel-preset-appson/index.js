837a1dc840524b865b098b2b50e08dfde0e2c8bc
Pedro Nauck <pedronauck@gmail.com>
2017-09-18 23:09:35 -0300

    chore: update packages dependencies

--- packages/babel-preset-appson/index.js
+++ packages/babel-preset-appson/index.js
@@ -5,7 +5,6 @@ const objectRestSpread = require('babel-plugin-transform-object-rest-spread')
 const reactJSX = require('babel-plugin-transform-react-jsx')
 const runtime = require('babel-plugin-transform-runtime')
 const reactOptimize = require('babel-preset-react-optimize')
-const macros = require('babel-macros')
 
 exports.default = {
   presets: [
@@ -13,7 +12,6 @@ exports.default = {
     react,
   ],
   plugins: [
-    macros,
     exportExtensions,
     [objectRestSpread, { useBuiltIns: true }],
     [reactJSX, { useBuiltIns: true }],
