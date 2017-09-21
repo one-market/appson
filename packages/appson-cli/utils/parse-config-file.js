ea1b15c2e12a5d68ca60b216b56b577ad270eed4
Pedro Nauck <pedronauck@gmail.com>
2017-09-21 17:42:57 -0300

    feat(appson-cli): add parse-config-file method

---
+++ packages/appson-cli/utils/parse-config-file.js
@@ -0,0 +1,13 @@
+const fs = require('fs')
+
+module.exports = (rcPath) => {
+  let file
+
+  try {
+    file = JSON.parse(fs.readFileSync(rcPath, 'utf8'))
+  } catch (error) {
+    file = null
+  }
+
+  return file
+}
