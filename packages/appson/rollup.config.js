d97bf1e9f5e831dba99b4a28e6ba867ce4b9de3c
Pedro Nauck <pedronauck@gmail.com>
2017-10-09 14:34:36 -0300

    fix(packages/appson): build

---
+++ packages/appson/rollup.config.js
@@ -0,0 +1,91 @@
+/* eslint camelcase: 0 */
+import visualizer from 'rollup-plugin-visualizer'
+import babel from 'rollup-plugin-babel'
+import resolve from 'rollup-plugin-node-resolve'
+import commonjs from 'rollup-plugin-commonjs'
+import sourceMaps from 'rollup-plugin-sourcemaps'
+import replace from 'rollup-plugin-replace'
+import uglify from 'rollup-plugin-uglify'
+import typescript from 'rollup-plugin-typescript'
+import sizes from 'rollup-plugin-sizes'
+import { minify } from 'uglify-es'
+import pkg from './package.json'
+
+const env = process.env.NODE_ENV
+const external = [
+  'react',
+  'react-dom',
+  'react-router',
+  'redux',
+  'redux-saga',
+  'redux-saga/effects',
+]
+
+const globals = {
+  'react': 'React',
+  'react-dom': 'reactDom',
+  'react-router': 'reactRouter',
+  'redux': 'redux',
+  'redux-saga': 'createSagaMiddleware',
+  'redux-saga/effects': 'effects',
+}
+
+const plugins = [
+  sizes(),
+  visualizer({
+    sourcemap: true,
+  }),
+  resolve({
+    jsnext: true,
+    extensions: ['.js', '.ts', '.tsx'],
+  }),
+  commonjs({
+    namedExports: {
+      '../../node_modules/react-hot-loader/index.js': ['AppContainer'],
+    },
+  }),
+  typescript({
+    typescript: require('typescript'),
+  }),
+  babel({
+    exclude: [
+      '../../node_modules/**',
+      './node_modules/**',
+    ],
+  }),
+  replace({
+    'process.env.NODE_ENV': JSON.stringify(env),
+  }),
+  sourceMaps(),
+  (env === 'production' && uglify({
+    compress: {
+      pure_getters: true,
+      unsafe: true,
+      unsafe_comps: true,
+      warnings: false,
+    },
+  }, minify)),
+]
+
+export default [{
+  external,
+  globals,
+  plugins,
+  input: './src/index.ts',
+  name: 'appson',
+  sourcemap: true,
+  output: [
+    { file: pkg.main, format: 'umd', exports: 'named' },
+    { file: pkg.browser, format: 'umd', exports: 'named' },
+  ],
+}, {
+  external,
+  globals,
+  plugins,
+  input: './src/index.ts',
+  sourcemap: true,
+  output: [
+    { file: pkg.module, format: 'es' },
+    { file: pkg['jsnext:main'], format: 'es' },
+  ],
+}]
